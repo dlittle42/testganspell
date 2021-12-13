08997556455b274960b4e48ca861eb73489b0d1e
Derek Little <dlittle42@gmail.com>
2021-12-13 16:16:27 -0500

    fix spaces

--- art_gan.py
+++ art_gan.py
@@ -89,13 +89,13 @@ def save_images(cnt, noise):
 generated_images = generator.predict(noise)
 generated_images = 0.5 * generated_images + 0.5
 image_count = 0
-    for row in range(PREVIEW_ROWS):
-        for col in range(PREVIEW_COLS):
-            r = row * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
-            c = col * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
-            image_array[r:r + IMAGE_SIZE, c:c +
-                        IMAGE_SIZE] = generated_images[image_count] * 255
-            image_count += 1
+for row in range(PREVIEW_ROWS):
+    for col in range(PREVIEW_COLS):
+        r = row * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
+        c = col * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
+        image_array[r:r + IMAGE_SIZE, c:c +
+                    IMAGE_SIZE] = generated_images[image_count] * 255
+        image_count += 1
 output_path = 'output'
     if not os.path.exists(output_path):
         os.makedirs(output_path)
