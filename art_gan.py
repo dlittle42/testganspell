9731766a03b86e67bbf3c629722ab7356d3186c5
Derek Little <dlittle42@gmail.com>
2021-12-14 15:41:30 -0500

    update indent

--- art_gan.py
+++ art_gan.py
@@ -93,22 +93,22 @@ def save_images(cnt, noise):
         PREVIEW_MARGIN + (PREVIEW_ROWS * (IMAGE_SIZE + PREVIEW_MARGIN)),
         PREVIEW_MARGIN + (PREVIEW_COLS * (IMAGE_SIZE + PREVIEW_MARGIN)), 3),
         255, dtype=np.uint8)
-generated_images = generator.predict(noise)
-generated_images = 0.5 * generated_images + 0.5
-image_count = 0
-for row in range(PREVIEW_ROWS):
-    for col in range(PREVIEW_COLS):
-        r = row * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
-        c = col * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
-        image_array[r:r + IMAGE_SIZE, c:c +
-                    IMAGE_SIZE] = generated_images[image_count] * 255
-        image_count += 1
-output_path = 'output'
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
-filename = os.path.join(output_path, f"trained-{cnt}.png")
-im = Image.fromarray(image_array)
-im.save(filename)
+    generated_images = generator.predict(noise)
+    generated_images = 0.5 * generated_images + 0.5
+    image_count = 0
+    for row in range(PREVIEW_ROWS):
+        for col in range(PREVIEW_COLS):
+            r = row * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
+            c = col * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
+            image_array[r:r + IMAGE_SIZE, c:c +
+                        IMAGE_SIZE] = generated_images[image_count] * 255
+            image_count += 1
+    output_path = 'output'
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    filename = os.path.join(output_path, f"trained-{cnt}.png")
+    im = Image.fromarray(image_array)
+    im.save(filename)
 
 
 image_shape = (IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)
